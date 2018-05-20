6d5c4e227826cc55beeae2d516900a4653332fe8
clickingmouse <clickingmouse@users.noreply.github.com>
2018-05-21 07:45:48 +0800

    express init

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello"));
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
